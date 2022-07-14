4c4703430e329caad039f77a1f2827b94088fa50
roman-morokhovskyi <roman_morokhovskyi@goodyear.com>
2022-07-14 13:54:52 +0300

    change api domain

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 export const getBaseAPIUrl = () => {
   return process.env.NODE_ENV === "production"
-    ? "https://interviewboom.com/api"
+    ? "https://api.interviewboom.com/api"
     : "http://localhost:3000/api";
 };
